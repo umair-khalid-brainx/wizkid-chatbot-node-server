6c25e23ea8294bd91caebe1d1e7a69c2c85a99a8
umair-khalid-brainx <umair.khalid@brainxtech.com>
2023-06-01 14:17:33 +0500

    new build

--- server.js
+++ server.js
@@ -4,19 +4,11 @@ const cors = require("cors")
 
 const app = express()
 const port = 5089
-const allowedOrigin = "*"
+const corsOptions = {
+  origin: ["http://localhost:5500", "http://localhost:5089", "https://wizkid-bot-backend.brainxdemo.com", "http://localhost:5173"],
+}
 
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (origin === allowedOrigin || !origin) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error("Not allowed by CORS"))
-//     }
-//   },
-// }
-
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.static(path.join(__dirname, "dist")))
 
 app.get("*", (req, res) => {
